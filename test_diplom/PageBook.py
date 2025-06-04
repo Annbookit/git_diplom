08c17677b1d5daee66e83ed9774d48c8f2b65f02
Annbookit <sebast57945@gmail.com>
2025-06-04 17:32:09 +0500

    diplom edits

--- test_diplom/PageBook.py
+++ test_diplom/PageBook.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-
 class PageBook:
     def __init__(self, browser):
         self.browser = browser
@@ -25,27 +24,25 @@ class PageBook:
 
 # поиск нужного товара
     def search_input(self, search_name):
-        self.browser.implicitly_wait(15)
         self.browser.find_element(By.CSS_SELECTOR, "input.search-form__input.search-form__input--search").send_keys(search_name)
         self.browser.find_element(By.XPATH, "//button[@aria-label='Найти']").click()
 
 # добавление товара в избранное
     def add_tab(self):
-        self.browser.implicitly_wait(15)
+        self.browser.find_element(By.CSS_SELECTOR, ".popmechanic-close").click()
         body = self.browser.find_element(By.TAG_NAME, 'body')
         body.send_keys(Keys.PAGE_DOWN)
         self.browser.find_element(By.XPATH, "//article[1]//div[5]//button[2]").click()
 
 # поиск нужного товара по каталогу
     def search_catalog(self):
-        self.browser.implicitly_wait(15)
         self.browser.find_element(By.XPATH, "//button[@class='chg-app-button chg-app-button--primary chg-app-button--m chg-app-button--blue catalog-btn header__catalog-menu catalog-btn header__catalog-menu']").click()
         self.browser.find_element(By.XPATH, "//span[contains(text(),'Художественная литература')]").click()
         self.browser.find_element(By.XPATH, "//span[@class='categories-level-menu__item-title'][contains(text(),'Афоризмы. Цитаты')]").click()
 
 # добавление понравившегося товара в корзину
     def add_cart(self):
-        self.browser.implicitly_wait(15)
+        self.browser.find_element(By.CSS_SELECTOR, ".popmechanic-close").click()
         body = self.browser.find_element(By.TAG_NAME, 'body')
         body.send_keys(Keys.PAGE_DOWN)
         self.browser.find_element(By.XPATH, "//article[@data-chg-product-brand='Яуза-пресс ООО']//button[@aria-label='false']").click()
@@ -53,26 +50,22 @@ class PageBook:
 
 # покупка на сайте выбранного товара
     def pay(self):
-        self.browser.implicitly_wait(15)
         self.browser.get("https://www.chitai-gorod.ru/cart")
         self.browser.find_element(By.XPATH, "(//button[@class='chg-app-button chg-app-button--primary chg-app-button--xl chg-app-button--brand-blue chg-app-button--block cart-sidebar__order-button cart-sidebar__order-button'])[1]").send_keys("Марк Аврелий")
-        # browser.find_element(By.CSS_SELECTOR, "svg.search-form__icon-search").click()
 
 # возврат на главную страницу
     def main_page(self):
-        self.browser.implicitly_wait(15)
+        self.browser.find_element(By.CSS_SELECTOR, ".popmechanic-close").click()
         self.browser.find_element(By.CSS_SELECTOR, ".header__logo-wrapper").click()
 
 # открытие карточки товара
     def page_product(self):
-        self.browser.implicitly_wait(15)
         body = self.browser.find_element(By.TAG_NAME, 'body')
         body.send_keys(Keys.PAGE_DOWN)
         self.browser.find_element(By.XPATH, "(//a[contains(text(),'Мой любимый sputnik')])[1]").click()
 
 # добавление отзыва на книгу
     def comment(self):
-        self.browser.implicitly_wait(15)
         self.browser.find_element(By.CSS_SELECTOR, ".popmechanic-close").click()
         self.browser.find_element(By.XPATH, "(//button[contains(text(),'Понятно, закрыть')])[1]").click()
         body = self.browser.find_element(By.TAG_NAME, 'body')
--- test_diplom/test_ui.py
+++ test_diplom/test_ui.py
@@ -3,6 +3,8 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
 
 from PageBook import PageBook
 
@@ -13,7 +15,7 @@ from PageBook import PageBook
 def driver():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.maximize_window()
-    driver.implicitly_wait(30)
+    driver.implicitly_wait(60)
     driver.get("https://www.chitai-gorod.ru/")
     yield driver
     driver.quit()
@@ -30,6 +32,10 @@ def test_search_product(driver):
         shop.cookies()
     with allure.step("Поиск товара в строке поиска"):
         shop.search_input("Марк Аврелий")
+    with allure.step("Проверяем что результаты поиска отображаются на странице"):
+        author_name = "Марк Аврелий"
+        first_product_title = driver.find_element(By.XPATH, "//span[contains(text(),'Марк Аврелий')]")
+        assert author_name in first_product_title.text, "Книга не найдена"        
 
 
 @allure.feature("READ")
@@ -38,14 +44,17 @@ def test_search_product(driver):
 def test_add_tab(driver):
     shop = PageBook(driver)
     with allure.step("Изменение города"):
-        shop.city()
+        shop.city()  
+    with allure.step("Скрытие информационных сообщений"):
+        shop.cookies()     
     with allure.step("Поиск товара в строке поиска"):
         shop.search_input("Марк Аврелий")
-    with allure.step("Скрытие информационных сообщений"):
-        shop.cookies()
     with allure.step("Добавление товара в закладки"):
         shop.add_tab()
-
+    with allure.step("Проверяем что результаты поиска добавляются в избранное"):
+        # first_product_title = driver.find_element(By.XPATH, "(//div[@class='vfm-swipe-banner-container'])[1]")
+        login_banner = driver.find_element(By.CSS_SELECTOR, ".ui-header-modal.auth-modal-header").is_displayed()
+        assert login_banner, "Окно для регистрации/входа не отображается"  
 
 @allure.feature("READ")
 @allure.severity("blocker")
@@ -54,6 +63,8 @@ def test_add_cart(driver):
     shop = PageBook(driver)
     with allure.step("Изменение города"):
         shop.city()
+    with allure.step("Скрытие информационных сообщений"):
+        shop.cookies()        
     with allure.step("Поиск товара через каталог"):
         shop.search_catalog()
     with allure.step("Скрытие информационных сообщений"):
@@ -62,6 +73,10 @@ def test_add_cart(driver):
         shop.add_cart()
     with allure.step("Оформление покупки товаров"):
         shop.pay()
+    with allure.step("Проверяем что результаты поиска добавляются в корзину"):
+        name_book = "Все афоризмы"
+        product_in_cart = driver.find_element(By.XPATH, "(//div[contains(text(),'Все афоризмы')])[1]")
+        assert name_book in product_in_cart.text, "Товара нет в корзине" 
 
 
 @allure.feature("READ")
@@ -79,6 +94,9 @@ def test_add_comment(driver):
         shop.page_product()
     with allure.step("Комментирование товара"):
         shop.comment()
+    with allure.step("Проверяем что появляется окно входа/регистрации"):
+        login_banner = driver.find_element(By.CSS_SELECTOR, ".ui-header-modal.auth-modal-header").is_displayed()
+        assert login_banner, "Окно для регистрации/входа не отображается"  
 
 
 @allure.feature("READ")
@@ -88,9 +106,12 @@ def test_return_main(driver):
     shop = PageBook(driver)
     with allure.step("Изменение города"):
         shop.city()
-    with allure.step("Поиск товара в строке поиска"):
-        shop.search_input("Марк Аврелий")
     with allure.step("Скрытие информационных сообщений"):
         shop.cookies()
+    with allure.step("Поиск товара в строке поиска"):
+        shop.search_input("Марк Аврелий")
     with allure.step("Переход на главную страницу"):
         shop.main_page()
+    with allure.step("Проверяем что результаты поиска добавляются в корзину"):
+        main_page = driver.current_url
+        assert main_page == "https://www.chitai-gorod.ru/", "Страница не открылась" 
